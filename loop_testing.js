418fb48f14ff795d3044856757d427f82165fcd8
Rephistorch <chris.ardelean@comcast.net>
2017-04-09 08:06:43 -0500

    Fix 2.0
    Different approach

--- loop_testing.js
+++ loop_testing.js
@@ -118,9 +118,6 @@ retryOnFail(function() {
     $browser.addHeader('User-Agent', UserAgent);
     console.log('Setting User-Agent to ' + UserAgent);
   }
-  
-  // Variables
-  var element;
 
   // Get browser capabilities and do nothing with it, so that we start with a then-able command
   // ### I'm not quite sure why this is necessary.  I'm going to see what starting on the browser.get does.
@@ -141,8 +138,8 @@ retryOnFail(function() {
   startScript()
   
   // Step2
-  .then(stepCheck(continueFlag, 2.1, 'Find clickElement "Electronics"', function(){element = $browser.waitForAndFindElement(By.linkText("Electronics"), StepTimeout);}))
-  .then(stepCheck(continueFlag, 2.2, 'Click Element "Electronics"', function(){ element.click(); }))
+  .then(stepCheck(continueFlag, 2.1, 'Find clickElement "Electronics"', function(){return $browser.waitForAndFindElement(By.linkText("Electronics"), StepTimeout);}))
+  .then(stepCheck(continueFlag, 2.2, 'Click Element "Electronics"', function(element){ element.click(); }))
 
   // Step 3
   .then(stepCheck(continueFlag, 3.1, 'Find clickElement "//div[@class=\'side-2\']//a[normalize-space(.)=\'Camera & photo\']"', $browser.waitForAndFindElement(By.xpath("//div[@class=\'side-2\']//a[normalize-space(.)=\'Camera & photo\']"), StepTimeout)))
